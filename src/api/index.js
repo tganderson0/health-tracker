5df325ecdd0a3d3a9396a3ba94c8b6b5a1390e9e
Taylor Anderson <tganderson0@gmail.com>
2023-05-09 13:19:09 -0600

    forgot to save

--- src/api/index.js
+++ src/api/index.js
@@ -1,3 +1,5 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
 import axios from 'axios'
 
 const methods = {
@@ -39,4 +41,22 @@ const api = {
   // DELETE example
   // name: r('delete', 'route')
 }
+
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC9z1KiaNJcxDMNqKPytaEyJyz007BPhUw",
+  authDomain: "health-tracker-c2db1.firebaseapp.com",
+  projectId: "health-tracker-c2db1",
+  storageBucket: "health-tracker-c2db1.appspot.com",
+  messagingSenderId: "530232649065",
+  appId: "1:530232649065:web:b4a2d56f2dfd3b52a6c9f8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
 export default api
